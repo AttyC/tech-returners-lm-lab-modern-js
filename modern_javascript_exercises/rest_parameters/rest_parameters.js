d5e8d719b31c77d1e49f7f1863dbe5860f508174
Atty Cronin <antoniajcronin@gmail.com>
2023-05-25 14:33:28 +0100

    implement rest parameters

--- modern_javascript_exercises/rest_parameters/rest_parameters.js
+++ modern_javascript_exercises/rest_parameters/rest_parameters.js
@@ -1,8 +1,13 @@
 // Instructions can be found in rest_parameters.md
 
-function add(a, b) {
-  // Add a loop here
-  return a + b;
+// Using Rest Parameters and some kind of forLoop (your choice!), 
+// refactor this function so that it can add together any
+// number of arguments and fix the failing test.
+
+function add(...args) {
+  let sum = 0;
+  args.forEach(arg => sum += arg)
+  return sum;
 }
 
 add(1, 2, 3, 4, 5);
